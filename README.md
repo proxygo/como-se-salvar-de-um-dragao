0675120c322a16c4bc45a7a002483bcb58ff7545
Victor Wildner <vwildner@redhat.com>
2023-06-02 20:20:57 -0300

    fix: update size to match mobile phones
    Co-authored-by: Proxy_go <iscila@outlook.com>

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  <img src=".github/assets/badge.png" width="230px" />
+</div>
+<br/>
+<div align="center">
+  <img src="https://img.shields.io/github/languages/count/vcwild/tcc-proxy?color=%23ff3cbe&style=flat-square" alt="languages" />
+  <img src="https://img.shields.io/github/license/vcwild/tcc-proxy?color=%23ff3cbe&style=flat-square" alt="license" />
+  <img src="https://img.shields.io/github/repo-size/vcwild/tcc-proxy?color=%23ff3cbe&style=flat-square" alt="repo size" />
+  <img src="https://img.shields.io/github/actions/workflow/status/vcwild/tcc-proxy/build.yml?branch=main&style=flat-square&color=%23ff3cbe" alt="build" />
+</div>
+
+# tcc-proxy
+
+This Proxy Go's final project. It is an interactive e-book history.
+
+## References
+
+- [Mental canvas](https://www.mentalcanvas.com/)
+
+## License
+
+This project is licensed with an All Rights Reserved license - see the [LICENSE](LICENSE) file for details.
--- utils.js
+++ utils.js
@@ -3,30 +3,30 @@
 function modal(param) {
 
    const modalContainer = document.getElementById('modal')
-   
-   if(param == 'open') {
+
+   if (param == 'open') {
       modalContainer.style.display = 'flex';
    } else if (param == 'close') {
       modalContainer.style.display = 'none';
    } else {
-      
+
    }
-   
+
 }
 
 function checkWidth(param) {
    const dialog = document.getElementById('dialog')
    const box = document.getElementById('box')
-   if(window.screen.width < window.screen.height) {
+
+   if (window.matchMedia("(orientation: portrait)").matches) {
       dialog.style.display = 'flex';
       box.classList.add("tremer");
-      box.addEventListener("animationend", function() {
+      box.addEventListener("animationend", function () {
          box.classList.remove("tremer");
-       });
+      });
       return;
    } else {
       dialog.style.display = 'none';
-      console.log(param)
       modal(param)
    }
 }
